242c20f188b5d79fb1a2f2e274aa6ae5495be098
Abel Sierra <tolerantx@gmail.com>
2023-12-16 15:21:20 -0600

    Add a couple of rspec tests

---
+++ spec/controllers/weather_forecast_controller_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+describe WeatherForecastController do
+  describe "GET #index" do
+    it { expect(response).to have_http_status(200) }
+
+    # This test takes a long time because it consumes the external APIs,
+    # the goal would be to stub the http request or use VCR
+    context "when the city_name parameter is present" do
+      before do
+        get :index, params: { city_name: "Monterrey" }
+
+      end
+
+      it "renders a list of cities" do
+        expect(JSON.parse(response.body)).not_to be_empty
+      end
+    end
+
+    context "when the city_name parameter is empty" do
+      before do
+        get :index
+      end
+
+      it "renders an empty array" do
+        expect(JSON.parse(response.body)).to be_empty
+      end
+    end
+  end
+end
